a6bcc8c31cb33b1b55c57177498c056924f23524
Welington Dias <33883532+Welin7@users.noreply.github.com>
2022-02-22 01:20:47 -0300

    Minimal api using dapper.
    Refactory the class ProductRepository including the class DapperDbConnection. Transfer all the logic of command to class DapperDbConnection.

---
+++ MinimalApiDapper/ComandQuery/DapperDbConnection.cs
@@ -0,0 +1,82 @@
+﻿using Dapper;
+using MinimalApiDapper.Factory;
+using MinimalApiDapper.Models;
+using System.Data;
+
+namespace MinimalApiDapper.ComandQuery
+{
+    public class DapperDbConnection
+    {
+        private readonly IDbConnection _dbConnection;
+        private int result;
+        private IEnumerable<Product> listProduct;
+
+        public DapperDbConnection()
+        {
+            _dbConnection = new SqlFactory().GetSqlConnection();
+            result = 0;
+        }
+
+        public bool ComandAdd(Product product)
+        {
+            var query = "INSERT INTO [DbProduct].[dbo].[Product] VALUES(@Name,@Quantity,@Price)";
+            var parameters = new { Name = product.Name, Quantity = product.Quantity, Price = product.Price };
+
+            using (_dbConnection)
+            {
+                result = _dbConnection.Execute(query, parameters);
+            }
+
+            return (result != 0 ? true : false);
+        }
+
+        public  bool DeleteComand(int id)
+        {
+            var query = "DELETE [DbProduct].[dbo].[Product] WHERE [Id] = @productid";
+            var parameters = new { productid = id };
+
+            using (_dbConnection)
+            {
+                result = _dbConnection.Execute(query, parameters);
+            }
+
+            return (result != 0 ? true : false);
+        }
+
+        public  IEnumerable<Product> ComandGetListProducts()
+        {
+            var query = "SELECT * FROM [DbProduct].[dbo].[Product]";
+
+            using (_dbConnection)
+            {
+                listProduct = _dbConnection.Query<Product>(query).ToList();
+            }
+
+            return listProduct;
+        }
+
+        public  Product ComandGetById(int id)
+        {
+            var parameters = new { productid = id };
+
+            using (_dbConnection)
+            {
+                var result = _dbConnection.QueryFirstOrDefault<Product>("SELECT * FROM [DbProduct].[dbo].[Product] WHERE [Id] = @productid", parameters);
+                return result;
+            }
+        }
+
+        public bool ComandUpdate(string name, int id)
+        {
+            var query = "UPDATE [DbProduct].[dbo].[Product] SET [Name] = @productName WHERE [Id] = @productid";
+            var parameters = new { productName = name, productid = id };
+
+            using (_dbConnection)
+            {
+                result = _dbConnection.Execute(query, parameters);
+            }
+
+            return (result != 0 ? true : false);
+        }
+    }
+}
--- MinimalApiDapper/Repositories/ProductRepository.cs
+++ MinimalApiDapper/Repositories/ProductRepository.cs
@@ -1,84 +1,49 @@
-﻿using Dapper;
-using MinimalApiDapper.Factory;
+﻿using MinimalApiDapper.ComandQuery;
 using MinimalApiDapper.Interfaces;
 using MinimalApiDapper.Models;
-using System.Data;
 
 namespace MinimalApiDapper.Repositories
 {
     public class ProductRepository : IProductRepository
     {
-        private readonly IDbConnection _dbConnection;
-
         public ProductRepository()
         {
-            _dbConnection = new SqlFactory().GetSqlConnection();
         }
 
         public bool Add(Product product)
         {
-            var query = "INSERT INTO [DbProduct].[dbo].[Product] VALUES(@Name,@Quantity,@Price)";
-            var parametres = new {Name = product.Name, Quantity = product.Quantity, Price = product.Price};
-
-            int result = 0;
-            using(_dbConnection)
-            {
-                result = _dbConnection.Execute(query, parametres);
-            }
-
-            return (result != 0 ? true : false);
+            var dbConnect = new DapperDbConnection(); 
+            bool result = dbConnect.ComandAdd(product);
+            return result;
         }
 
         public bool Delete(int id)
         {
-            var query = "DELETE [DbProduct].[dbo].[Product] WHERE [Id] = @productid";
-            var parameters = new { productid = id };
-
-            int result = 0;
-            using(_dbConnection)
-            {
-                result = _dbConnection.Execute(query, parameters);
-            }
-
-            return (result != 0 ? true:false);
+            var dbConnect = new DapperDbConnection();
+            bool result = dbConnect.DeleteComand(id);
+            return result;
         }
 
         public IEnumerable<Product> GetProducts()
         {
             var products = new List<Product>();
-            var query = "SELECT * FROM [DbProduct].[dbo].[Product]";
-
-            using (_dbConnection)
-            {
-                products = _dbConnection.Query<Product>(query).ToList();
-            }
-
+            var dbConnect = new DapperDbConnection();
+            products = (List<Product>)dbConnect.ComandGetListProducts();
             return products;
         }
 
         public Product GetById(int id)
         {
-            var parameters = new { productid = id };
-
-            using (_dbConnection)
-            {
-                var result = _dbConnection.QueryFirstOrDefault<Product>("SELECT * FROM [DbProduct].[dbo].[Product] WHERE [Id] = @productid", parameters);
-                return result;
-            }
+            var dbConnect = new DapperDbConnection();
+            var result = dbConnect.ComandGetById(id);  
+            return (Product)result;
         }
 
         public bool Update(string name, int id)
         {
-            var query = "UPDATE [DbProduct].[dbo].[Product] SET [Name] = @productName WHERE [Id] = @productid";
-            var parameters = new { productName = name, productid = id };
-
-            int result = 0;
-            using (_dbConnection)
-            {
-                result = _dbConnection.Execute(query, parameters);
-            }
-
-            return (result != 0 ? true : false);
+            var dbConnect = new DapperDbConnection();
+            bool result = dbConnect.ComandUpdate(name, id); 
+            return result;
         }
     }
 }
